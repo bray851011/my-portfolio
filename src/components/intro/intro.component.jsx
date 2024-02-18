dc7fdedea66d1966f893353ae9a85cfc152054d6
Po Jui Juan <bray851011@gmail.com>
2024-02-17 21:01:22 -0500

    Update Navbar, SideBar, and Intro

--- src/components/intro/intro.component.jsx
+++ src/components/intro/intro.component.jsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+import { TypeAnimation } from 'react-type-animation';
+import { motion } from 'framer-motion';
+
 import facebook from '../../assets/facebook.svg'
 import github from '../../assets/github.svg'
 import leetcood from '../../assets/leetcode.svg'
@@ -9,29 +12,94 @@ import scroll from '../../assets/scroll.svg'
 import './intro.styles.scss'
 
 const Intro = () => {
+
+    const textVariants = {
+        initial: {
+            x: -500,
+            opacity: 0,
+        },
+        animate: {
+            x: 0,
+            opacity: 1,
+            transition: {
+                duration: 1,
+                staggerChildren: 0.3,
+            }
+        },
+        scrollButton: {
+            opacity: 0,
+            y: 15,
+            transition: {
+                duration: 3,
+                repeat: Infinity,
+            }
+        }
+    }
+
+    const imgVariants = {
+        initial:{ 
+            opacity: 1
+        },
+        animate:{
+            opacity: 1, // Fade in to full opacity
+            y: [0, -50, 0], // Move up by 50px and then back down
+            transition:{
+                duration: 3, // Duration of one cycle (up and down)
+                ease: "easeInOut", // Easing function for the animation
+                repeat: Infinity, // Repeat the animation indefinitely
+                repeatType: "loop", // Loop the animation
+            }
+        }
+    }
+
     return (
         <div className='intro-container'>
             <div className='wrapper'>
-                <div className='text-container'>
-                    <h2>Ray Juan</h2>
-                    <h1>Software Developer and Graduate Student</h1>
-                    <h3>A self-motivated and hardworking individual that has the ability to pick things up quick and learn new knowledge in a fast pace. Passionate in solving problems and not afraid of tackling challenges.</h3>
-                    <div className='buttons'>
-                        <button>My resume</button>
+                <motion.div 
+                    className='text-container'
+                    variants={textVariants}
+                    initial="initial"
+                    animate="animate"
+                >
+                    <motion.h1 variants={textVariants}>Ray Juan</motion.h1>
+                    <motion.div variants={textVariants}>
+                        <TypeAnimation
+                            sequence={[
+                                // Same substring at the start will only be typed out once, initially
+                                'A Software Engineer',
+                                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                                'A Graduate Student',
+                                1000,
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            style={{ fontSize: '30px', display: 'inline-block', fontWeight: "bold",  fontFamily: "Black Ops One"}}
+                            repeat={Infinity}
+                        />
+                    </motion.div>
+                    
+                    <motion.h2 variants={textVariants}>A self-motivated and hardworking individual that has the ability to pick things up quick and learn new knowledge in a fast pace. Passionate in solving problems and not afraid of tackling challenges.</motion.h2>
+                    <motion.div className='buttons' variants={textVariants}>
+                        <button>Check out my resume</button>
                         <button>Hire Me</button>
                         <Link to='https://www.linkedin.com/in/ray-juan/'><img className="icon" src={linkedin} alt=""/></Link>
                         <Link to=''><img className="icon" src={github} alt=""/></Link>
                         <Link to=''><img className="icon" src={leetcood} alt=""/></Link>
                         <Link to=''><img className="icon" src={facebook} alt=""/></Link>
                         <Link to=''><img className="icon" src={instagram} alt=""/></Link>
-                    </div>
-                    <img className='scroll' src={scroll} alt=''/>
-                </div>
+                    </motion.div>
+                    <motion.img variants={textVariants} animate="scrollButton" className='scroll' src={scroll} alt=''/>
+                </motion.div>
 
-                <div className='image-container'>
-                    <img src={mypic} alt=''/>
-                </div>
-            </div>            
+                <motion.div 
+                    className='image-container' 
+                    variants={imgVariants} 
+                    initial='initial' 
+                    animate='animate'
+                >
+                    <motion.img src={mypic} alt=''/>
+                </motion.div>
+            </div>          
         </div>
     )
 }
--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,15 +1,36 @@
 import './navbar.styles.scss'
-
+import { motion } from 'framer-motion';
 import Sidebar from "../sidebar/sidebar.component";
 
 const Navbar = () => {
+
+    const variants = {
+        initial: {
+            y: -200,
+            opacity: 0,
+        },
+        animate: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                duration: 1,
+                staggerChildren: 0.3,
+
+            }
+        }
+    }
+
     return (
-        <div className='navbar-container'>
-            <div className='wrapper'>
-                <h1>Ray Juan</h1>
-            </div>
+        <motion.div className='navbar-container' 
+            variants={variants}
+            initial="initial"
+            animate="animate"
+        >
+            <motion.div className='wrapper'>
+                <motion.h1>Ray's Portfolio</motion.h1>
+            </motion.div>
             <Sidebar />
-        </div>
+        </motion.div>
     )
 }
 
