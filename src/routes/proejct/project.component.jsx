aa3aa0aae6566b87cc2cd751fa134c7d0d04b902
Po Jui Juan <bray851011@gmail.com>
2024-02-20 17:12:04 -0500

    Update Project Component

--- src/routes/proejct/project.component.jsx
+++ src/routes/proejct/project.component.jsx
@@ -1,9 +1,12 @@
 
+import Navbar from '../../components/navbar/navbar.component';
 import './project.styles.scss'
 
 const Project = () => {
     return (
-        <div></div>
+        <div className=''>
+            <Navbar /> 
+        </div>
     )
 }
 
