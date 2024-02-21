5b71c24e83577aa2d333f4eba3dd984ab5b2d589
Po Jui Juan <bray851011@gmail.com>
2024-02-20 22:04:08 -0500

    Update Contact Component"

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Home from './routes/home/home.component';
 import Project from './routes/proejct/project.component';
+import Contact from './routes/contact/contact.component';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Route path="/" >
         <Route index element={<Home />}/>
         <Route path='/project' element={<Project />}/>
+        <Route path='/contact' element={<Contact />}/>
       </Route>
     </Routes>
   )
